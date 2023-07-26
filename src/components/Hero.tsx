deb2332dcc0fb6f140ea259a604aea2d67caf0f0
kir4che <mollydcxxiii@gmail.com>
2023-07-26 15:42:33 +0800

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,48 +10,42 @@ const Hero = ({ movies }: { movies: Movie[] }) => {
 
   const navigate = useNavigate();
 
-  function reviews(movieId: any) {
+  // 當點擊「Reviews」按鈕時，導航到相應的評論頁面。
+  function reviews(movieId: string) {
     navigate(`/Reviews/${movieId}`);
   }
 
   return (
-    <div className='movie-carousel-container'>
-      <Carousel>
-        {
-          movies?.map((movie) => {
-            return (
-              <Paper key={movie.imdbId}>
-                <div className='movie-card-container'>
-                  <div className="movie-card">
-                    <div className="movie-detail">
-                      <div className="movie-poster">
-                        <img src={movie.poster} alt="" />
-                      </div>
-                      <div className="movie-title">
-                        <h4>{movie.title}</h4>
-                      </div>
-                      <div className="movie-buttons-container">
-                        <Link to={`/Trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}>
-                          <div className="play-button-icon-container">
-                            <FontAwesomeIcon className="play-button-icon"
-                              icon={faCirclePlay}
-                            />
-                          </div>
-                        </Link>
+    <Carousel>
+      {
+        movies?.map((movie) => {
+          const bgImg = `url(${movie.backdrops[0]})`;
 
-                        <div className="movie-review-button-container">
-                          <button onClick={() => reviews(movie.imdbId)} >Reviews</button>
-                        </div>
-                      </div>
+          return (
+            <Paper key={movie.imdbId}>
+              <div className='h-[550px] bg-primary'>
+                <div className="w-full h-full bg-no-repeat bg-cover" style={{ backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0), rgba(0,0,0,1)), ${bgImg}` }}>
+                  <div className="absolute flex w-full top-32 justify-evenly">
+                    <div className="w-[300px] overflow-hidden">
+                      <img className='w-full h-full' src={movie.poster} alt="" />
+                    </div>
+                    <div className="flex items-center">
+                      <h2 className='text-4xl font-black tracking-wide text-white'>{movie.title}</h2>
+                    </div>
+                    <div className='flex items-center space-x-6 w-80'>
+                      <Link to={`/Trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}>
+                        <FontAwesomeIcon className="text-6xl text-white" icon={faCirclePlay} />
+                      </Link>
+                      <button className="px-5 py-1.5 font-bold rounded-full bg-secondary" onClick={() => reviews(movie.imdbId)} >Reviews</button>
                     </div>
                   </div>
                 </div>
-              </Paper>
-            )
-          })
-        }
-      </Carousel>
-    </div>
+              </div>
+            </Paper>
+          )
+        })
+      }
+    </Carousel >
   )
 }
 
