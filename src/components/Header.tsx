af61de63c40c89b525612aea9ce6809cf73be33f
kir4che <mollydcxxiii@gmail.com>
2023-07-26 15:42:30 +0800

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,27 @@
 import { faVideoSlash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
 
   return (
-    <header>
-      <div className="container">
-        <div>
-          <FontAwesomeIcon icon={faVideoSlash} />Gold
+    <header className="container mx-auto">
+      <div className="flex items-center py-4 space-x-5">
+        <div className="flex items-center space-x-1 text-secondary">
+          <FontAwesomeIcon icon={faVideoSlash} />
+          <p className="text-xl">Gold</p>
         </div>
         <div />
-        <div>
-          <div className="my-2 me-auto my-lg-0">
-            <a className="nav-link" to="/">Home</a>
-            <a className="nav-link" to="/watchList">Watch List</a>
+        <div className="flex justify-between w-full">
+          <div className="my-2 space-x-5">
+            <Link to="/">Home</Link>
+            <Link to="/watchList">Watch List</Link>
           </div>
-          <button>Login</button>
-          <button>Register</button>
+          <div className="flex items-center space-x-3">
+            <button className="px-3 py-1 font-bold rounded-md border-[1px] border-secondary text-secondary hover:bg-secondary hover:text-bgColor">Login</button>
+            <button className="px-3 py-1 font-bold rounded-md border-[1px] border-secondary text-secondary hover:bg-secondary hover:text-bgColor">Register</button>
+          </div>
         </div>
       </div>
     </header>
