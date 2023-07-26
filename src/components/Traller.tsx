ccc1f1228506127f318545a4a566ab2807a15457
kir4che <mollydcxxiii@gmail.com>
2023-07-26 15:42:45 +0800

    Update Traller.tsx

--- src/components/Traller.tsx
+++ src/components/Traller.tsx
@@ -1,7 +1,7 @@
+import React from 'react';
+
 import ReactPlayer from 'react-player';
 import { useParams } from 'react-router-dom';
-
-import React from 'react';
 
 const Trailer = () => {
 
@@ -9,9 +9,11 @@ const Trailer = () => {
   let key = params.ytTrailerId;
 
   return (
-    <div className="react-player-container">
-      {(key != null) ? <ReactPlayer playing={true} url={`https://www.youtube.com/watch?v=${key}`}
-        width='100%' height='100%' /> : null}
+    <div className="h-[90vh]">
+      {
+        (key != null) ? <ReactPlayer playing={true} url={`https://www.youtube.com/watch?v=${key}`}
+          width='100%' height='100%' /> : null
+      }
     </div>
   )
 }
