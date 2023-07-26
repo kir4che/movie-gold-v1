e71b8f15e246dd64bb6e012041b4ba061ac82373
kir4che <mollydcxxiii@gmail.com>
2023-07-26 15:42:46 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ['./src/**/*.{html,js,jsx,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#232631',
+				secondary: '#FFE826',
+				bgColor: '#191A22',
+			},
+		},
+	},
+	plugins: [],
 }
-
