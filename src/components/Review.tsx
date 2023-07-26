ac4fe1251b571fff7661d46dfc16fae56d519d6e
kir4che <mollydcxxiii@gmail.com>
2023-07-26 15:42:34 +0800

    Update Review.tsx

--- src/components/Review.tsx
+++ src/components/Review.tsx
@@ -4,49 +4,35 @@ import api from '../api/axiosConfig';
 import ReviewForm from './ReviewForm';
 
 import React from 'react';
-
-interface Review {
-  body: string;
-}
-
-interface Movie {
-  poster: string;
-  // Add other properties of the movie object if available
-}
+import { Movie, Review } from '../type';
 
 interface Props {
   getMovieData: (movieId: string) => void;
-  movie: Movie | null;
-  reviews: Review[];
+  movie: Movie | undefined;
+  reviews: Review[]
   setReviews: (reviews: Review[]) => void;
 }
 
 const Reviews: React.FC<Props> = ({ getMovieData, movie, reviews, setReviews }) => {
-  const revText = useRef<HTMLInputElement>(null); // Use HTMLInputElement for ref type
+  const revText = useRef<HTMLInputElement>(null);
   const params = useParams<{ movieId: string }>();
   const movieId = params.movieId;
 
   useEffect(() => {
-    if (movieId) {
-      getMovieData(movieId);
-    }
-  }, [getMovieData, movieId]); // Don't forget to add movieId to the dependency array
+    if (movieId) getMovieData(movieId);
+  }, [getMovieData, movieId]);
 
   const addReview = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (!revText.current) return; // Make sure the ref is valid
-
+    if (!revText.current) return;
     const rev = revText.current.value;
 
     try {
       if (movieId) {
         const response = await api.post("/api/v1/reviews", { reviewBody: rev, imdbId: movieId });
-
         const updatedReviews = [...reviews, { body: rev }];
-
         revText.current.value = "";
-
         setReviews(updatedReviews);
       }
     } catch (err) {
@@ -55,53 +41,25 @@ const Reviews: React.FC<Props> = ({ getMovieData, movie, reviews, setReviews }) 
   };
 
   return (
-    <div className='container'>
-      <div>
-        <div>
-          <h3>Reviews</h3>
-        </div>
-      </div>
-      <div className="mt-2">
-        <div>
-          <img src={movie?.poster} alt="" />
-        </div>
-        <div>
-          {movie && ( // Add a condition to render only when movie is available
-            <>
-              <div>
-                <div>
-                  <ReviewForm handleSubmit={addReview} revText={revText} labelText="Write a Review?" defaultValue={undefined} />
-                </div>
-              </div>
-              <div>
-                <div>
-                  <hr />
-                </div>
-              </div>
-            </>
+    <div className='container px-20 pt-4 mx-auto'>
+      <h3 className='mb-4 text-3xl'>Reviews</h3>
+      <div className="flex items-start justify-between">
+        <img className='h-[74vh]' src={movie?.poster} alt="" />
+        <div className='w-[640px]'>
+          {movie && (
+            <ReviewForm handleSubmit={addReview} revText={revText} labelText="Write a Review?" defaultValue={undefined} />
           )}
           {
-            reviews.map((r: Review, index: number) => ( // Use map directly and provide a unique key to avoid warning
+            reviews.map((r: Review, index: number) => (
               <React.Fragment key={index}>
-                <div>
-                  <div>{r.body}</div>
-                </div>
-                <div>
-                  <div>
-                    <hr />
-                  </div>
-                </div>
+                <div>{r.body}</div>
               </React.Fragment>
             ))
           }
         </div>
       </div>
-      <div>
-        <div>
-          <hr />
-        </div>
-      </div>
-    </div>
+      <hr />
+    </div >
   )
 }
 
